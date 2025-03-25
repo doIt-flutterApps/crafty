5d2a54ee8c21dd457084a1ae1517e6bdfd8ccd67
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-03-25 15:35:38 +0900

    (초판) 08-2-1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,12 +14,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8" // JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -27,10 +28,11 @@ android {
         applicationId = "com.example.crafty"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23 // flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        ndkVersion = "27.0.12077973"
     }
 
     buildTypes {
@@ -44,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4") // Or the latest version
 }
---
+++ lib/data/constant.dart
@@ -0,0 +1,7 @@
+class Constant {
+  static const APP_NAME = 'Crafty';
+}
+enum SignType {
+  Email,
+  Google,
+}
